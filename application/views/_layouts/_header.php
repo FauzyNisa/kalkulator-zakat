bf9f18fa8ffec45e98207bdb79390ed7546f8f3c
Yuda Aditya <yudaaditya98@gmail.com>
2020-02-09 21:40:24 +0700

    Template, Bootstrap dan warna

---
+++ application/views/_layouts/_header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title><?php echo $title ?></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap-grid.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap-reboot.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('theme/bootstrap/css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('theme/custom/css/custom-style.css') ?>">
+</head>
+
+<body>
